8b8e1280a01f6c9f07b9fe2da2676e1156b30e76
Aliou S Maiga <badrasadja@gmail.com>
2020-06-02 01:48:01 -0700

    Initial commit

---
+++ test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celcius table
+    for fahr = 0, 20, ..., 300
+    */
+
+main(){
+  float fahr, celcius;
+  int lower, upper, step;
+
+  lower = 0;        /* lower limit of temperature table */
+  upper = 300;     /*  upper limit */
+  step = 10;      /*   step size */
+
+  celcius = lower;
+  printf("Celcius - Fahr\n");
+  while (celcius <= upper){
+    fahr = 32.0 + (9.0 * celcius) / 5;
+    printf("%4.0f   %7.1f\n", celcius, fahr);
+    celcius = celcius + step;
+  }
+
+}
